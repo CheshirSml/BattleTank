9d7bd6a021bd92643cc5321f165c6750757ec17a
CheshirSml <Cheshir.gorbachenko1@gmail.com>
2018-06-07 12:25:18 +0300

    add linetracesinglebychannel

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -36,5 +36,10 @@ private:
 	UPROPERTY(EditAnywhere)
 		float CrosshairYLocation = 0.3333;
 
-	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	UPROPERTY(EditAnywhere)
+		float LineTraceRange = 10000000;
+
+		bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+		bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 };
+
